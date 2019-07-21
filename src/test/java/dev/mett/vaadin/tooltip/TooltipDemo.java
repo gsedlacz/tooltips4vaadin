b29ad173e30cb01569318c1438998d44aa1aea2d
Gerrit Sedlaczek <gerrit.sedlaczek@web.de>
2019-07-21 22:46:21 +0200

    version: 1.1.1 -> 1.1.2 + prepared internal demo

---
+++ src/test/java/dev/mett/vaadin/tooltip/TooltipDemo.java
@@ -0,0 +1,38 @@
+package dev.mett.vaadin.tooltip;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class TooltipDemo extends HorizontalLayout {
+	private static final long serialVersionUID = 7591127437515385460L;
+	private final AtomicLong atomicLong = new AtomicLong();
+
+	public TooltipDemo() {
+        EmailField emailField = new EmailField();
+        Tooltips.setTooltip(emailField, "initial Value");
+
+        Button btChangeTooltip = new Button("change tooltip", evt -> {
+        	Tooltips.setTooltip(emailField, "value-" + atomicLong.getAndIncrement());
+        });
+
+        Button btRemoveTooltip = new Button("remove tooltip", evt -> {
+        	Tooltips.removeTooltip(emailField);
+        });
+
+        Button btDetachAttachField = new Button("detach/attach field", evt -> {
+        	if(getChildren().filter(c -> emailField == c).count() != 0) {
+        		remove(emailField);
+        	} else {
+        		getElement().insertChild(0, emailField.getElement());
+        	}
+        });
+
+        add(emailField, new VerticalLayout(btChangeTooltip, btRemoveTooltip, btDetachAttachField));
+	}
+}
