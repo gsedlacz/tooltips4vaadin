67c6debd24bb001b8f2dd263e66a70cb6a5b4bd9
Gerrit Sedlaczek <gerrit.sedlaczek@web.de>
2019-08-20 18:02:25 +0000

    Dev to main

--- src/main/java/dev/mett/vaadin/tooltip/TooltipStateData.java
+++ src/main/java/dev/mett/vaadin/tooltip/TooltipStateData.java
@@ -1,7 +1,9 @@
 package dev.mett.vaadin.tooltip;
 
+import java.lang.ref.WeakReference;
 import java.util.Optional;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.shared.Registration;
 
 import lombok.Getter;
@@ -16,4 +18,6 @@ public class TooltipStateData {
 	private String cssClass;
 	@Getter @Setter
 	private String tooltip;
+	@Getter @Setter
+	private WeakReference<Component> component;
 }
