691ebe03f886bb75826ad24f354a5167ef817c66
gsedlacz <gerrit.sedlaczek@web.de>
2019-08-20 18:15:37 +0200

    vaadin version: 14.0.1

--- src/main/java/dev/mett/vaadin/tooltip/TooltipsJsProvider.java
+++ src/main/java/dev/mett/vaadin/tooltip/TooltipsJsProvider.java
@@ -3,10 +3,11 @@ package dev.mett.vaadin.tooltip;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 
 @NpmPackage(value = "tippy.js", version = "4.3.4")
-@JavaScript("./js/tooltip/tooltips.js")
+@JsModule("./js/tooltip/tooltips.js")
 @Tag("div")
 public class TooltipsJsProvider extends Component {
 	private static final long serialVersionUID = 3079421969338830944L;
--- webpack.generated.js
+++ webpack.generated.js
@@ -11,7 +11,7 @@ const {BabelMultiTargetPlugin} = require('webpack-babel-multi-target-plugin');
 const path = require('path');
 const baseDir = path.resolve(__dirname);
 // the folder of app resources (main.js and flow templates)
-const frontendFolder = `${baseDir}/frontend`;
+const frontendFolder = require('path').resolve(__dirname, 'frontend');
 
 const fileNameOfTheFlowGeneratedMainEntryPoint = require('path').resolve(__dirname, 'target/frontend/generated-flow-imports.js');
 const mavenOutputFolderForFlowBundledFiles = require('path').resolve(__dirname, 'target/classes/META-INF/VAADIN');
@@ -64,6 +64,9 @@ module.exports = {
     after: function(app, server) {
       app.get(`/stats.json`, function(req, res) {
         res.json(stats.toJson());
+      });
+      app.get(`/stats.hash`, function(req, res) {
+        res.json(stats.toJson().hash.toString());
       });
       app.get(`/stop`, function(req, res) {
         // eslint-disable-next-line no-console
