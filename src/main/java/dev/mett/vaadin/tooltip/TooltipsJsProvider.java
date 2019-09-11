942753c8f7f3fefd57dc8a3546e1bcce50676959
gsedlacz <gerrit.sedlaczek@web.de>
2019-09-11 19:48:49 +0200

    #5: WIP added retry library

--- src/main/java/dev/mett/vaadin/tooltip/TooltipsJsProvider.java
+++ src/main/java/dev/mett/vaadin/tooltip/TooltipsJsProvider.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 
 @NpmPackage(value = "tippy.js", version = "4.3.4")
+@NpmPackage(value = "retry", 	version = "0.12.0")
 @JsModule("./js/tooltip/tooltips.js")
 @Tag("div")
 public class TooltipsJsProvider extends Component {
--- src/main/resources/META-INF/resources/frontend/js/tooltip/tooltips.js
+++ src/main/resources/META-INF/resources/frontend/js/tooltip/tooltips.js
@@ -1,8 +1,9 @@
 import tippy from 'tippy.js';
+import retry from 'retry';
 
 window.tooltips = {
 		/* Util */
-		getElement: function(classname) {
+		getElement: function (classname) {
 			return document.querySelector('.' + classname);
 		},
 		
@@ -11,10 +12,14 @@ window.tooltips = {
 			if(tooltipElement) tippy(tooltipElement, {
 				content: tooltip
 			});
+			
+			const abc = retry.operation();
+			
+			// this id will be used by tooltips DOM id associated with the tooltipElement
 			return tooltipElement._tippy.id;
 		},
 		
-		updateTooltip: function(classname, tooltip){
+		updateTooltip: function (classname, tooltip){
 			const tooltipElement = window.tooltips.getElement(classname);
 			if(tooltipElement) tooltipElement._tippy.setContent(tooltip);
 		},
@@ -27,5 +32,18 @@ window.tooltips = {
 			   gets removed during the open animation */
 			const lostTooltip = document.getElementById('tippy-' + tooltipId);
 			if(lostTooltip) lostTooltip._tippy.destroy();
+		},
+
+		/*
+			https://www.wolframalpha.com/input/?i=Sum%5B100*x%5Ek%2C+%7Bk%2C+0%2C+4%7D%5D+%3D+5+*+1000
+			https://www.wolframalpha.com/input/?i=Sum%5B100*2.3178%5Ek%2C+%7Bk%2C+0%2C+4%7D%5D
+		*/
+		getRetryOperation = function (){
+			return retry.operation({
+				retries: 5,
+				factor: 2.3178,
+				minTimeout: 100,
+				maxTimeout: 1000
+			  });
 		}
 }
