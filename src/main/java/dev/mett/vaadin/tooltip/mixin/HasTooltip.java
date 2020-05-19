bea332f10e38005c91bf42d467c241808a2b5eb6
Gerrit Sedlaczek <gerrit.sedlaczek@web.de>
2020-05-19 17:20:02 +0200

    #2: TooltipConfig support for HasTooltip

--- src/main/java/dev/mett/vaadin/tooltip/mixin/HasTooltip.java
+++ src/main/java/dev/mett/vaadin/tooltip/mixin/HasTooltip.java
@@ -5,51 +5,77 @@ import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.UI;
 
+import dev.mett.vaadin.tooltip.TooltipConfiguration;
 import dev.mett.vaadin.tooltip.Tooltips;
 
 /**
  * Allows to define Tooltips more easily by adding this mixin interface.
- * 
+ *
  * @author Gerrit Sedlaczek
  */
 public interface HasTooltip extends HasElement {
-	
-	/**
-	 * Adds a tooltip to the implementing {@link Component}.
-	 * 
-	 * @param tooltip the String to display (may contain html)
-	 */
-	default void setTooltip(String tooltip) {
-		setTooltip(tooltip, UI.getCurrent());
-	}
-	
-	/**
-	 * Adds a tooltip to the implementing {@link Component}.
-	 * 
-	 * @param <T> the type of a styleable component
-	 * @param tooltip the String to display (may contain html)
-	 * @param ui {@link UI}
-	 */
-	@SuppressWarnings("unchecked")
-	default <T extends Component & HasStyle> void setTooltip(String tooltip, UI ui) {
-		getElement().getComponent().ifPresent(comp -> {
-			if(comp instanceof HasStyle) {
-				Tooltips.get(ui).setTooltip((T)comp, tooltip);
-			}
-		});
-	}
-	
-	/**
-	 * Removes a tooltip from the implementing {@link Component}
-	 * 
-	 * @param <T> the type of a styleable component
-	 * @param ui {@link UI}
-	 */
-	@SuppressWarnings("unchecked")
-	default <T extends Component & HasStyle> void removeTooltip(UI ui) {
+
+    /**
+     * Adds a tooltip to the implementing {@link Component}.
+     *
+     * @param tooltipConfig the configuration describing a tooltip
+     */
+    default void setTooltip(TooltipConfiguration tooltipConfig) {
+        setTooltip(tooltipConfig, UI.getCurrent());
+    }
+
+    /**
+     * Adds a tooltip to the implementing {@link Component}.
+     *
+     * @param <T>           the type of a styleable component
+     * @param tooltipConfig the configuration describing a tooltip
+     * @param ui            {@link UI}
+     */
+    @SuppressWarnings("unchecked")
+    default <T extends Component & HasStyle> void setTooltip(TooltipConfiguration tooltipConfig, UI ui) {
         getElement().getComponent().ifPresent(comp -> {
-            if(comp instanceof HasStyle) {
-                Tooltips.get(ui).removeTooltip((T)comp);
+            if (comp instanceof HasStyle) {
+                Tooltips.get(ui).setTooltip((T) comp, tooltipConfig);
+            }
+        });
+    }
+
+    /**
+     * Adds a tooltip to the implementing {@link Component}.
+     * 
+     * @param tooltip the String to display (may contain html)
+     */
+    default void setTooltip(String tooltip) {
+        setTooltip(tooltip, UI.getCurrent());
+    }
+
+    /**
+     * Adds a tooltip to the implementing {@link Component}.
+     * 
+     * @param <T>     the type of a styleable component
+     * @param tooltip the String to display (may contain html)
+     * @param ui      {@link UI}
+     */
+    @SuppressWarnings("unchecked")
+    default <T extends Component & HasStyle> void setTooltip(String tooltip, UI ui) {
+        getElement().getComponent().ifPresent(comp -> {
+            if (comp instanceof HasStyle) {
+                Tooltips.get(ui).setTooltip((T) comp, tooltip);
+            }
+        });
+    }
+
+    /**
+     * Removes a tooltip from the implementing {@link Component}
+     * 
+     * @param <T> the type of a styleable component
+     * @param ui  {@link UI}
+     */
+    @SuppressWarnings("unchecked")
+    default <T extends Component & HasStyle> void removeTooltip(UI ui) {
+        getElement().getComponent().ifPresent(comp -> {
+            if (comp instanceof HasStyle) {
+                Tooltips.get(ui).removeTooltip((T) comp);
             }
         });
     }
