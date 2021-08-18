51cc66a0f342fd578c88ec1d8a80dc99c7e6855c
Gerrit Sedlaczek <gerrit.sedlaczek@web.de>
2021-08-18 17:02:11 +0000

    #37: possible fix - multiple tooltips on a single element

--- src/main/resources/META-INF/resources/frontend/js/tooltip/tooltips.js
+++ src/main/resources/META-INF/resources/frontend/js/tooltip/tooltips.js
@@ -12,7 +12,7 @@ window.tooltips = {
    *  - https://www.wolframalpha.com/input/?i=Sum%5B100*x%5Ek%2C+%7Bk%2C+0%2C+4%7D%5D+%3D+5+*+1000
    *  - https://www.wolframalpha.com/input/?i=Sum%5B100*2.3178%5Ek%2C+%7Bk%2C+0%2C+4%7D%5D
   */
-  getRetryOperation: function () {
+  _getRetryOperation: function () {
     return retry.operation({
       retries: 5,
       factor: 2.3178,
@@ -21,16 +21,16 @@ window.tooltips = {
     });
   },
 
-  getElement: function (frontendId) {
+  _getElement: function (frontendId) {
     return document.querySelector('[tt4v=' + frontendId + ']');
   },
 
-  getElementFaulttolerant: async function (frontendId) {
-    const operation = this.getRetryOperation();
+  _getElementFaulttolerant: async function (frontendId) {
+    const operation = this._getRetryOperation();
 
     return new Promise((resolve, reject) => {
       operation.attempt(async function () {
-        const element = window.tooltips.getElement(frontendId);
+        const element = window.tooltips._getElement(frontendId);
         const err = element === undefined || element == null
             ? "Could not find element for class: " + frontendId : null;
 
@@ -47,12 +47,30 @@ window.tooltips = {
     });
   },
 
+  _setupTippyPlugins: function (config) {
+    config.plugins = [];
+
+    // enables required plugins
+    if (config.followCursor) {
+      config.plugins.push(followCursor);
+    }
+    if (config.sticky) {
+      config.plugins.push(sticky);
+    }
+  },
+
+  _removeTooltipFromElement: function (tooltipElement) {
+    if (tooltipElement) {
+      tooltipElement._tippy.destroy();
+    }
+  },
+
   /* ### INTERACTION ### */
 
   setTooltip: function (frontendId, config) {
-    return this.getElementFaulttolerant(frontendId)
+    return this._getElementFaulttolerant(frontendId)
     .then(tooltipElement => {
-      return window.tooltips.setTooltipToElement(tooltipElement, config);
+      return this.setTooltipToElement(tooltipElement, config);
     })
     .catch(err => {
       console.warn("setTooltip: " + err);
@@ -62,16 +80,11 @@ window.tooltips = {
 
   setTooltipToElement: function (tooltipElement, config) {
     if (tooltipElement) {
-      config.plugins = [];
-
-      // enables required plugins
-      if (config.followCursor) {
-        config.plugins.push(followCursor);
-      }
-      if (config.sticky) {
-        config.plugins.push(sticky);
+      if(tooltipElement._tippy){
+        this._removeTooltipFromElement(tooltipElement)
       }
 
+      this._setupTippyPlugins(config);
       tippy(tooltipElement, config);
 
       // this id will be used by tooltips DOM id associated with the tooltipElement
@@ -80,15 +93,16 @@ window.tooltips = {
   },
 
   updateTooltip: function (frontendId, config) {
-    return this.getElementFaulttolerant(frontendId)
+    return this._getElementFaulttolerant(frontendId)
     .then(tooltipElement => {
       if (tooltipElement) {
         if (tooltipElement._tippy) {
+          this._setupTippyPlugins(config)
           tooltipElement._tippy.setProps(config);
 
         } else {
           // lost its _tippy sub entry for some reason
-          return window.tooltips.setTooltipToElement(tooltipElement, config);
+          return this.setTooltipToElement(tooltipElement, config);
         }
       }
     })
@@ -98,15 +112,11 @@ window.tooltips = {
   },
 
   removeTooltip: function (frontendId, tooltipId) {
-    window.tooltips.closeTooltipForced(tooltipId);
+    this.closeTooltipForced(tooltipId);
 
     /* destroy frontend tooltip on the element */
-    this.getElementFaulttolerant(frontendId)
-    .then(tooltipElement => {
-      if (tooltipElement) {
-        tooltipElement._tippy.destroy();
-      }
-    })
+    this._getElementFaulttolerant(frontendId)
+    .then(this._removeTooltipFromElement)
     .catch(err => {
       console.warn("removeTooltip: " + err);
     })
@@ -116,10 +126,8 @@ window.tooltips = {
   closeTooltipForced: function (tooltipId) {
     /* tippy fails to remove tooltips whose registered component
        gets removed during the open animation */
-    const lostTooltip = document.getElementById('tippy-' + tooltipId);
-    if (lostTooltip) {
-      lostTooltip._tippy.destroy();
-    }
+    const tooltipElement = document.getElementById('tippy-' + tooltipId);
+    this._removeTooltipFromElement(tooltipElement)
   },
 
   closeAllTooltips: function () {
